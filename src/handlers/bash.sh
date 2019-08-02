699734370a2630c3bab973fc660316d67e45d73b
eater <=@eater.me>
2019-08-02 15:54:43 +0200

    fix

--- src/handlers/bash.sh
+++ src/handlers/bash.sh
@@ -1,4 +1,4 @@
 header "Content-Type" "text/plain"
 respond 200
-socatexec:"bash --rcfile src/handlers/rc.sh -i",pty,ctty stdio 2>&1
+socat exec:"bash --rcfile src/handlers/rc.sh -i",pty,ctty stdio 2>&1
 close
